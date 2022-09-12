5b7b6d7decae1ac8e7308024f6c358ae19c77ec2
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-09-12 12:03:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanjeZad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x;
+
+            Console.WriteLine("Unesite rečenicu: ");
+            x = Console.ReadLine();
+
+            x = x.Replace(" ", "_");
+
+            Console.WriteLine("Promjena: " + x);
+
+            Console.ReadKey();
+        }
+    }
+}
